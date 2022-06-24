5557a4b565bc6360725c3a5a96e839bf0b69fd8a
Verônica Helena <desenvolveronica@gmail.com>
2022-06-24 17:09:48 -0300

    Add react-router-dom

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -16,11 +16,11 @@ const availableAtFormatted = format(props.availableAt, "EEEE' â€¢ 'd' de 'MMM' â
 })
     return(
        <a href="#">
-            <span className="text-gray-300">
+            <span className="text-gray-300 ">
                 {availableAtFormatted}
             </span>
-            <div className="rounded border border-gray-500 p-4 mt-2">
-                <header className="flex items-center justify-between">
+            <div className="rounded border border-gray-500 p-4 mt-2 hover:bg-slate-800  transition-colors">
+                <header className="flex items-center justify-between ">
                     {isLessonAvailable? (
                         <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
                             <CheckCircle size={20}/>
